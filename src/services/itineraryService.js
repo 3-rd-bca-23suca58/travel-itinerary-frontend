6a27d3525ea33da7d48dda4bcfef06a667eca654
Kaviyaswedha R <kaviyaswedha.r21@gmail.com>
2026-01-31 18:51:27 +0530

    itineraryService.js

--- src/services/itineraryService.js
+++ src/services/itineraryService.js
@@ -1,10 +1,2 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:8080/api/itineraries";
-
-export const getItineraries = () => axios.get(API_URL);
-
-export const addItinerary = (data) => axios.post(API_URL, data);
-
-export const deleteItinerary = (id) =>
-  axios.delete(`${API_URL}/${id}`);
+export const updateItinerary = (id, data) =>
+  axios.put(`${API_URL}/${id}`, data);
